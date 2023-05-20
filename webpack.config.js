8d13470762493269c15c052e51e50d7c7aca52ca
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-05-20 04:51:34 +0100

    Configure babel and setup database with postgresql

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,19 @@ module.exports = {
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 }
