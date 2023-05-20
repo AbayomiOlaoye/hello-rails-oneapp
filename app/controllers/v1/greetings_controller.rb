9abb2ae0330c27b6e8f84d1b47d3bae9c0e9003c
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-05-20 09:42:54 +0100

    Fix linters

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 class V1::GreetingsController < ApplicationController
   def index
     @greeting = Greeting.order('RANDOM()').first
-      render json: { greeting: @greeting.message }
+    render json: { greeting: @greeting.message }
   end
 end
